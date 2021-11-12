1f17c2999df8ef878adeecd0a26866b78e3ef5fd
xenial <admin@xenial.xyz>
2021-11-12 14:27:46 +0200

    Add USACO/671

---
+++ USACO/Gold/Lasers_and_Mirrors.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define fi first
+#define se second
+#define sz size
+#define rsz resize
+#define ii pair<int,int>
+
+void set_io(string filename = "") {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    if (filename != "") {
+        freopen((filename + ".in").c_str(), "r", stdin);
+        freopen((filename + ".out").c_str(), "w", stdout);
+    }
+}
+
+int main() {
+    set_io("");
+
+    int N;
+    ii laser, barn;
+    cin >> N >> laser.fi >> laser.se >> barn.fi >> barn.se;
+
+    vector<int> adj[N + 2];
+
+    
+}
