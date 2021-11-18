a5d2a4c0ffd162a311564e0dc782bfee7fa1419f
xenial <admin@xenial.xyz>
2021-11-18 15:55:05 +0200

    Add CSES/Problemset/1202
    https://cses.fi/problemset/task/1202

---
+++ CSES/Problemset/Graphs/Investigation.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define fi first
+#define se second
+#define sz size
+#define rsz resize
+#define ii pair<int,int>
+
+void set_io(string filename = "") {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    if (filename != "") {
+        freopen((filename + ".in").c_str(), "r", stdin);
+        freopen((filename + ".out").c_str(), "w", stdout);
+    }
+}
+
+ll price[100005], routes[100005];
+int mod = 1e9 + 7, minflights[100005], maxflights[100005];
+bool visited[100005];
+
+int main() {
+    set_io("");
+
+    fill_n(&price[0], 100005, 1e18);
+    fill_n(&minflights[0], 100005, 1e9);
+
+    int N, M; cin >> N >> M;
+    vector<ii> adj[N + 1];
+
+    while (M--) {
+        int a, b, c; cin >> a >> b >> c;
+        adj[a].pb({b, c});
+    }
+
+    priority_queue<pair<ll, int>> q;
+    q.push({0, 1});
+    routes[1] = 1;
+    minflights[1] = maxflights[1] = 0;
+
+    while (!q.empty()) {
+        auto c = q.top();    
+        q.pop();
+
+        ll cp = -c.fi;
+        int cn = c.se;
+
+        if (visited[cn]) continue;
+        visited[cn] = true;
+
+        for (auto n : adj[cn]) {
+            minflights[n.fi] = min(minflights[n.fi], minflights[cn] + 1);
+            maxflights[n.fi] = max(maxflights[n.fi], maxflights[cn] + 1);
+
+            routes[n.fi] = max(routes[n.fi], routes[cn]);
+
+            if (price[n.fi] == cp + n.se) routes[n.fi] = (routes[n.fi] + routes[cn] % mod) % mod;
+            price[n.fi] = min(price[n.fi], cp + n.se);
+
+            q.push({-price[n.fi], n.fi});
+        }
+    }
+
+    cout << price[N] << " " << routes[N] << " " << minflights[N] << " " << maxflights[N] << endl;
+}
