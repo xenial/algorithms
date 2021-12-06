7130109fac4f44af29b81d1f1970abb12334e921
sk <ohzegix@gmail.com>
2021-12-06 19:29:52 +0200

    Solve AoC/5
    https://adventofcode.com/2021/day/5

---
+++ AoC/2021/5/B.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define fi first
+#define se second
+#define sz size
+#define rsz resize
+#define ii pair<int,int>
+
+void set_io(string filename = "") {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    if (filename != "") {
+        freopen((filename + ".in").c_str(), "r", stdin);
+        freopen((filename + ".out").c_str(), "w", stdout);
+    }
+}
+
+int grid[1005][1005];
+
+int main() {
+    set_io("");
+
+    for (int i = 0; i < 500; i++) {
+        ii x, y;
+        cin >> x.fi >> y.fi >> x.se >> y.se;
+
+        int dir = 2; // 0 = x, 1 = y, 2 = xy
+        if (x.fi == x.se) dir = 1;
+        else if (y.fi == y.se) dir = 0;
+
+        if (dir == 0) {
+            if (x.fi > x.se) swap(x.fi, x.se);
+            for (int j = x.fi; j <= x.se; j++) grid[j][y.fi]++;
+        } else if (dir == 1) {
+            if (y.fi > y.se) swap(y.fi, y.se);
+            for (int j = y.fi; j <= y.se; j++) grid[x.fi][j]++;
+        } else if (dir == 2) {
+            if (x.fi > x.se) {
+                swap(x.fi, x.se);
+                swap(y.fi, y.se);
+            }
+            int yd = y.fi < y.se ? 1 : -1;
+            for (int j = 0; abs(j) <= x.se - x.fi; j += yd) grid[x.fi + abs(j)][y.fi + j]++;
+        }
+    }
+
+    int ans = 0;
+    for (int i = 0; i < 1001; i++) 
+        for (int j = 0; j < 1001; j++) 
+            if (grid[i][j] > 1) ans++;
+
+    cout << ans << endl;
+}
