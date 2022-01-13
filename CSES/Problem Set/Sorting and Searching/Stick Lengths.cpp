abe1c34c1f6ee0d7123e4a3ce4f347926f7bb80d
sk <ohzegix@gmail.com>
2022-01-13 18:46:00 +0200

    Solve CSES/Problemset/1074
    https://cses.fi/problemset/task/1074

---
+++ CSES/Problem Set/Sorting and Searching/Stick Lengths.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define fi first
+#define se second
+#define sz size
+#define rsz resize
+#define ii pair<int,int>
+
+void set_io(string filename = "") {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    if (filename != "") {
+        freopen((filename + ".in").c_str(), "r", stdin);
+        freopen((filename + ".out").c_str(), "w", stdout);
+    }
+}
+
+int main() {
+	set_io("");
+
+    int n; cin >> n;
+    vector<ll> sticks(n);
+
+    for (auto &s : sticks) cin >> s;
+    sort(all(sticks));
+
+    ll med;
+    if (n % 2) med = (sticks[n / 2] + sticks[n / 2 + 1]) / 2;
+    else med = sticks[n / 2];
+
+    ll ans = 0;
+    for (auto s : sticks) ans += abs(s - med);
+    cout << ans << endl;
+}
