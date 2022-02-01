bae7bb11340374c62d9a108b0efb30558f104f9e
sk <ohzegix@gmail.com>
2022-02-01 11:18:22 +0200

    Solve CF/1626A
    https://codeforces.com/contest/1626/problem/A

---
+++ Codeforces/1626/A.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define fi first
+#define se second
+#define sz size
+#define rsz resize
+#define ii pair<int,int>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+
+void set_io(string filename = "") {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    if (filename != "") {
+        freopen((filename + ".in").c_str(), "r", stdin);
+        freopen((filename + ".out").c_str(), "w", stdout);
+    }
+}
+
+int main() {
+	set_io("");
+
+    int T; cin >> T;
+
+    while (T--) {
+        string s; cin >> s;
+        sort(all(s));
+        cout << s << endl;
+    }
+}
